6d27424a5fb9cab4f9b46ae4475a432f1b02f045
Sam Plews <the@codeite.com>
2024-12-07 14:38:50 +0000

    Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+export function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="max-w-7xl mx-auto py-4 lg:py-12 px-4 sm:px-6 lg:px-8">
+        <div className="bg-white rounded-lg shadow-lg p-8">
+          <h1 className="text-4xl font-bold text-gray-900 mb-4">
+            Lunch Log
+          </h1>
+          <p className="text-lg text-gray-600">
+            A website for Sam to log where he goes for lunch
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+} 
